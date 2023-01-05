76c26b7f84407ad9d28cd47a7c2f4c237e021fa6
league <league@imac-22.attlocal.net>
2023-01-04 16:56:57 -0800

    1/4/22 Today, I did change calculator and test scores.

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -12,17 +12,20 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
-		// Convert their answer to an int.   Hint: Integer.parseInt()  
-
+         String nickels = JOptionPane.showInputDialog("How many nickels do you have");
+         int nickel = Integer.parseInt(nickels);
+         // Convert their answer to an int.   Hint: Integer.parseInt()  
+        
 		// Ask the user how many dimes they have, and convert their answer
-
+         String dimes = JOptionPane.showInputDialog("How many dimes do you have");
+         int dime = Integer.parseInt(dimes);
 		// Ask the user how many quarters they have, and convert their answer
-
-		// Calculate how much money the user has.  Hint: Use a double variable 
-
+           String quarters = JOptionPane.showInputDialog("How many quaters do you have");
+	       int quarter = Integer.parseInt(quarters);
+           // Calculate how much money the user has.  Hint: Use a double variable 
+         double money = dime*0.10 + nickel*0.05 + quarter*0.25; 
 		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
-
+        JOptionPane.showMessageDialog(null,"$" + money);
 	}
 }
 
