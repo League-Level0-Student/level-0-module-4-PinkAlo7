ed3c518b59047c086d06aa30c554fbc883c22e3c
league <ameeralih786@gmail.com>
2023-01-11 16:48:25 -0800

    1/11/23 Today, I did the rest of test scores, sleepy head, star student, game over, and red square.

--- src/_01_double/_2_test_scores/TestScores.java
+++ src/_01_double/_2_test_scores/TestScores.java
@@ -5,12 +5,18 @@ import javax.swing.JOptionPane;
 public class TestScores {
 public static void main(String[] args) {
 	String test = JOptionPane.showInputDialog("What is your test score in decimal form?");
-	 int score= Integer.parseInt(test);
-	if(score >= 75.5) {
+	 double score = Double.parseDouble(test);
+	if(score >= 75.0) {
 		JOptionPane.showMessageDialog(null,"Wow, great job! You must have studied really hard for that test!!");
 	}
-	if(score >= 50.3) {
-		JOptionPane.showMessageDialog(null,"Hmmm, not really what I was expecting, but I guess that score is a little bit good.");
+	else if(score >= 50.0) {
+		JOptionPane.showMessageDialog(null,"Hmmm, not really what I was expecting, that score is not so good.");
+	}
+	else if(score >=20.0) {
+		JOptionPane.showMessageDialog(null,"Wow, that score is soooooooooooooooooooooooo BAAAAAAD!!!!!!!!!!!");
+	}
+	else if(score >=0.0) {
+		JOptionPane.showMessageDialog(null,"Wow, I'm very sorry that you got such a low score, but that is the most horrible test score I've ever heard!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 	}
 }
 }
--- src/_02_boolean/_4_red_square/RedSquare.java
+++ src/_02_boolean/_4_red_square/RedSquare.java
@@ -24,6 +24,17 @@ public class RedSquare {
 		if(isRed && isSquare) {
 			drawRedSquare();
 		}
+		
+		else if(!isRed && isSquare) {
+			drawBlueSquare();
+		}
+		else if(isRed && !isSquare) {
+			drawRedTriangle();
+		}
+		else if(!isRed && !isSquare) {
+			drawBlueTriangle();
+		}
+		
 		else {
             JOptionPane.showMessageDialog(null, "No shape was drawn!");
         }
@@ -32,17 +43,17 @@ public class RedSquare {
 		
 		// 2. Initialize isRed to true instead of false and run the program 
 		//    again.
-		
+		isRed = true;
 		// 3. Write an else if statement after the if statement that calls 
 		//    drawBlueSquare if isRed is false AND isSquare is true. 
 		//    Hint: Use ! and &&.
-		
+	
 		// 4. Write an else if statement that calls drawRedTriangle() if isRed
 		//    is true and isSquare is false.
 		
 		// 5. Write an else if statement that calls drawBlueTriangle if both
 		//    isRed and isSquare are false.
-		
+	
 		// 6. Keep rerunning your program, initializing isRed and isSquare to 
 		//    different values until you've seen a red square, red triangle, 
 		//    blue square and blue triangle drawn.
