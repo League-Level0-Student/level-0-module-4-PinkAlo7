c62f1fba03072a6fef34e8f1b4c5b69775f524ca
league <ameeralih786@gmail.com>
2023-04-12 17:01:34 -0700

    4/12/23 Today I did 99extra pong.

--- src/_99_extra/pong/Pong.java
+++ src/_99_extra/pong/Pong.java
@@ -1,11 +1,17 @@
 package _99_extra.pong;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Pong extends PApplet {
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
    int x=50;
+   int y=20;
+   int xspeed = 9;
+   int yspeed = 9;
+   PImage backgroundImage;
+   
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
@@ -13,15 +19,34 @@ public class Pong extends PApplet {
 
     @Override
     public void setup() {
-
+    	backgroundImage = loadImage("pongbackground.jpg");
+    	backgroundImage.resize(800, 600);
     }
 
     @Override
     public void draw() {
+background(backgroundImage);
 fill(245, 66, 179);
 stroke(206, 66, 245);
-ellipse(50,50,20,20);
-    }
+ellipse(x,y,20,20);
+x=x+xspeed;
+if(x > width||x<0) {
+	xspeed =-xspeed;
+	
+}
+y=y+yspeed;
+if(y > height||y<0) {
+	yspeed=-yspeed;
+}
+if(mousePressed){
+    println("Mouse’s x-position: " + mouseX + "\n" + "Mouse’s y-position: " + mouseY + "\n");
+}
+fill(255, 0, 0);
+stroke(255,0,0);
+rect(mouseX,mouseY,80,5);
+}
+
+
 
     static public void main(String[] args) {
         PApplet.main(Pong.class.getName());
